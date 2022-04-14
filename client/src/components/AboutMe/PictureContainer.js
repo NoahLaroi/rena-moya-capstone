47cafb2eef7912f14b3b63616a4661e89a24661f
NoahLaroi <noahll597@gmail.com>
2022-04-14 17:38:43 -0400

    created develop branch/made about me page

--- client/src/components/AboutMe/PictureContainer.js
+++ client/src/components/AboutMe/PictureContainer.js
@@ -1,9 +1,9 @@
 export default function PictureContainer () {
     return (
-        <section className='pictureContainer'>
-            <div className='level levelOne'></div>
-            <div className='level levelTwo'></div>
-            <div className='level levelThree'></div>
-        </section>
+        <section className='pictureContainer'>   
+        <div className='topPic pic-one'></div>
+        <div className='topPic pic-two'></div>
+        <div className='topPic pic-three'></div>
+   </section>
     )
 }
--- client/src/components/Gallery/Gallery.js
+++ client/src/components/Gallery/Gallery.js
@@ -1,9 +1,12 @@
 import React from "react"
-
+import './gallery.scss'
+import handOnHip from '../../assets/images/handonhip.jpg'
+import closedSmile from '../../assets/images/serenaclosedsmile.jpg'
+import sittingDown from '../../assets/images/serenasitting.jpg'
 export default function Gallery () {
     return (
         <section className='gallery'>
-           
+     
         </section>
     )
 } 
