39d48acac44e2e561f93a739d59e96d31361c38e
NoahLaroi <noahll597@gmail.com>
2022-04-21 16:12:10 -0400

    added instagram embed and updated scheduling form

--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
 const { PORT, BACKEND_URL } = process.env;
-const cors = require ("cors")
 const app = express();
+const cors = require ("cors")
 require("dotenv").config();
 app.use(cors());
 app.use(express.json())
 app.listen()
 const port = 8080;
-const { v4 } = require("uuid");
 
 function readAppointments() {
     const appointments = fs.readFileSync("./data/appointments.json");
@@ -18,18 +18,51 @@ function readAppointments() {
   }
 const appointmentData = readAppointments();
 
+function readTimeSlots() {
+  const timeSlots = fs.readFileSync("./data/timeSlots.json");
+  const parsedData = JSON.parse(timeSlots);
+  return parsedData;
+}
+const timeSlots = readTimeSlots();
+
 function writeApppointment(data) {
     fs.writeFileSync("./data/appointments.json", JSON.stringify(data));
+    (err) => {
+      if (err) throw err;
+    };
   }
-  
+  console.log(timeSlots)
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
   })
-
-router
-.route ('/')
-.get((req, res)=> {
-let appointmentData = writeApppointment();
-res.status(200).send(appointmentData);
+app.get ('/', function (req, res) {
+  res.send(appointmentData)
+}
+)
+app.get('/timeslots', function (req, res) {
+  res.send(timeSlots)
 })
- 
+// router
+// .route ('/')
+// .get((req, res)=> {
+// let appointmentData = writeApppointment();
+// res.status(200).send(appointmentData);
+// })
+app.post('/', function (req, res) {
+  console.log(req.body);
+  const { name, phone, date, time, description} = req.body;
+  const newAppointment = {
+    id: uuidv4(),
+    name: name,
+    phome: phone,
+    date: date,
+    time: time,
+    description: description,
+  };
+  console.log(appointmentData);
+  const updatedAppointmentData = [...appointmentData, newAppointment];
+writeApppointment(updatedAppointmentData);
+})
+app.use(function (req,res,next){
+	res.status(404).send('Unable to find the requested resource!');
+});
